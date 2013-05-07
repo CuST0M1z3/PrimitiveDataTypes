6137b3a62534274786f3ecb349eedef5af566165
CuST0M1z3 <iordan.bonev@gmail.com>
2013-05-07 18:23:33 +0300

    Create 10. MarketingFirm.cs

---
+++ 10. MarketingFirm.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MarketingFirm
+{
+    static void Main()
+    {
+        string firstName = "Ivan";
+        string lastName = "Ivanov";
+        int age = 20;
+        string gender = "male";
+        int idNumber = 22;
+        int employeeNumber = 27560030;
+
+        Console.WriteLine("FirstName: " + firstName);
+        Console.WriteLine("LastName: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Gender: " + gender);
+        Console.WriteLine("ID: " + idNumber);
+        Console.WriteLine("EmployeeNumber: " + employeeNumber);
+    }
+}
