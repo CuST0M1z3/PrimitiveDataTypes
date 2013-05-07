2de630bed2a79ec2097f5a5043d2dcd52aaf955d
CuST0M1z3 <iordan.bonev@gmail.com>
2013-05-07 18:22:46 +0300

    Create 8. StringVariables.cs

---
+++ 8. StringVariables.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+class StringVariables
+{
+    static void Main()
+    {
+
+        string withQuotes = "The \"use\" of quotations causes difficulties.";
+        string withoutQuotes = "The use of quotations causes difficulties.";
+
+        Console.WriteLine(withQuotes);
+        Console.WriteLine(withoutQuotes);
+
+    }
+}
